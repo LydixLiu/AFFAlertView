e6ea8bd99410a742f13487865fbdaed61517fae9
liuxuancheng <liuxuancheng@lightinthebox.com>
2018-03-15 10:40:46 +0800

    implement show and dismiss function

--- AFFAlertView/AFFAlertView/AFFAlertViewController.m
+++ AFFAlertView/AFFAlertView/AFFAlertViewController.m
@@ -6,10 +6,14 @@
 //  Copyright © 2018年 Mystic-Liu. All rights reserved.
 //
 
+#define key_vc  "com.alert.vc"
+
 #import "AFFAlertViewController.h"
+#import <objc/runtime.h>
 
 @interface AFFAlertViewController () {
     alertBlock _block;
+    UIWindow *_window;
 }
 
 @property (nonatomic, copy) NSString *alertTitle;
@@ -65,15 +69,48 @@
 }
 
 - (void)showInMainWindow {
+    if (!_window) {
+        _window = [UIApplication sharedApplication].keyWindow;
+    }
     
+    [_window addSubview:self.view];
+    objc_setAssociatedObject(_window, key_vc, self, OBJC_ASSOCIATION_RETAIN);
+    self.view.alpha = 0;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.view.alpha = 1;
+    }];
 }
 
 - (void)showInViewController:(UIViewController *)vc {
+    self.showingVC = vc;
     
+    [vc.view addSubview:self.view];
+    self.view.alpha = 0;
+    
+    [vc addChildViewController:self];
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.view.alpha = 1;
+    }];
 }
 
 - (void)dismiss {
-    
+    if (self.showingVC) {
+        [UIView animateWithDuration:0.2 animations:^{
+            self.view.alpha = 0;
+        } completion:^(BOOL finished) {
+            [self.view removeFromSuperview];
+            [self removeFromParentViewController];
+        }];
+    } else {
+        [UIView animateWithDuration:0.2 animations:^{
+            self.view.alpha = 0;
+        } completion:^(BOOL finished) {
+            [self.view removeFromSuperview];
+            objc_setAssociatedObject(_window, key_vc, nil, OBJC_ASSOCIATION_RETAIN);
+        }];
+    }
 }
 
 @end
